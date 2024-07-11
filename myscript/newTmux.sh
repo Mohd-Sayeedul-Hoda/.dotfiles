482a2c8460542b78e0f744518f13f8f5b82a6c21
Sayeed <sayeedulhoda@gmail.com>
2024-07-12 02:10:20 +0530

    fix newTmux script

--- myscript/newTmux.sh
+++ myscript/newTmux.sh
@@ -2,6 +2,7 @@
 
 selected_dir=$(fd -H --base-directory=/home/aman  --type directory --ignore-file .myScript/ignoreFile.txt | fzy)
 if [ -n "$selected_dir" ]; then
-    cd "$selected_dir" || exit 1
-    tmux new-session -c "$selected_dir"
+    cd "/home/aman/$selected_dir" || exit 1
+    current_dir=$(pwd)
+    tmux new-session -c "$current_dir"
 fi
